e3a033a23fd9489e8935f0d0d5841c58d70f8ed4
Pamela Fox <pamela.fox@gmail.com>
2022-07-25 15:16:31 -0700

    First commit

---
+++ ChainFunction/__init__.py
@@ -0,0 +1,35 @@
+import azure.functions as func
+
+from ChainFunction.markov_chain import MarkovChain
+
+def get_param(req, param_name):
+    param_value = req.params.get(param_name)
+    if not param_value:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            param_value = req_body.get(param_name)
+    return param_value
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    kind = get_param(req, 'kind')
+    chain = MarkovChain()
+    if kind == "planet":
+        chain.load_from_disk('ChainFunction/planet0825.pickle')
+        chain.generate_word()
+        return func.HttpResponse(
+            chain.generate_word(),
+            status_code=200
+        )
+    elif kind:
+        return func.HttpResponse(
+             f"Unknown chain kind: {kind}",
+             status_code=404
+        )
+    else:
+        return func.HttpResponse(
+             "Chain kind not specified.",
+             status_code=404
+        )
---
+++ scraper.py
@@ -0,0 +1,54 @@
+import re
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+from markov_chain import MarkovChain
+
+def parse_bookpage(url):
+    # https://www.gutenberg.org/ebooks/64317
+    try:
+        response = urllib.request.urlopen(url)
+    except urllib.error.HTTPError:
+        print('ERROR: Book page could not be opened', url)
+
+    html = response.read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Find the files table
+    contents = soup.find('table', class_='files')
+    # Find the plain text link
+    link = contents.find('a', class_='link', type='text/plain')
+    if not link:
+        link = contents.find('a', class_='link', type='text/plain; charset=utf-8')
+    text_url = f"https://www.gutenberg.org{link['href']}"
+    print(text_url)
+    try:
+        response = urllib.request.urlopen(text_url)
+    except urllib.error.HTTPError:
+        print('ERROR: Book text could not be opened', text_url)
+
+    text = response.read().decode('utf-8')
+    booktext = re.split(r'\*\*\* START OF [\w\s]+ \*\*\*', text, 1)[1]
+    return booktext
+
+def create_planet_chain():
+    # Just once
+    # nltk.download('brown')
+    # nltk.download('punkt')
+    markov_chain = MarkovChain()
+    urls = [
+        "https://www.gutenberg.org/ebooks/23306",
+        "https://www.gutenberg.org/ebooks/46768",
+        "https://www.gutenberg.org/ebooks/16561",
+        "https://www.gutenberg.org/ebooks/8400",
+        "https://www.gutenberg.org/ebooks/19072"
+    ]
+    for url in urls:
+        print(url)
+        booktext = parse_bookpage(url)
+        markov_chain.add_text(booktext)
+    names = []
+    for _ in range(0, 10):
+        names.append(markov_chain.generate_word())
+
